670711d9b81f018de4652b8328e81bc431ed72a6
Shivamgarg581 <shivgarg184@gmail.com>
2026-01-07 15:51:45 +0530

    Create creators.js

---
+++ creators.js
@@ -0,0 +1,5 @@
+const creators=[
+ {name:"TechReels", niche:"Tech", reach:"250K", rate:"₹8000/post", location:"Delhi"},
+ {name:"FoodiesHub", niche:"Food", reach:"180K", rate:"₹6000/post", location:"Mumbai"},
+ {name:"TravelWithMe", niche:"Travel", reach:"300K", rate:"₹10,000/post", location:"Goa"}
+];
